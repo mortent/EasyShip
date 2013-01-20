3bdbe618724db2f1c2abe37ee2ff75bf7fe8a44f
Morten Torkildsen <morten.torkildsen@bekk.no>
2013-01-19 19:46:35 -0500

    Files that didn't get commited last time

--- Website/easy_ship/models.py
+++ Website/easy_ship/models.py
@@ -1,17 +1,26 @@
 from django.db import models
 
-class ServiceLevels(models.Model):
+class ServiceLevel(models.Model):
 	name = models.CharField(max_length=100)
+	
+	def __unicode__(self):
+		return self.name
 
-class ZoneChart(models.Model):
+class Zone(models.Model):
 	zip_code = models.CharField(max_length=3)
-	service_level = models.ForeignKey(ServiceLevels)
-	zone = models.CharField(max_length=3)
+	service_level = models.ForeignKey(ServiceLevel)
+	zone_number = models.CharField(max_length=3)
+	
+	def __unicode__(self):
+		return "Zip code: " + self.zip_code + ", Service Level: " + self.service_level
 
-class RateChart(models.Model):
+class Rate(models.Model):
 	weight = models.IntegerField()
-	service_level = models.ForeignKey(ServiceLevels)
-	zone = models.CharField(max_length=3)
+	service_level = models.ForeignKey(ServiceLevel)
+	zone_number = models.CharField(max_length=3)
 	price = models.IntegerField()
+	
+	def __unicode__(self):
+		return "Weight: " + self.weight + ", Service level: " + self.service_level + ", Zone: " + self.zone
 
 	

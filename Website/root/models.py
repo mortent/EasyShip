4e3b70d73020f4e6e15927166e13395bf19d3b28
Morten Torkildsen <morten.torkildsen@bekk.no>
2013-01-24 15:41:14 -0500

    Changes

--- Website/root/models.py
+++ Website/root/models.py
@@ -1,36 +1,41 @@
 from django.db import models
 import datetime
 from django.forms import ModelForm
+
 class ServiceLevel(models.Model):
-	name = models.CharField(max_length=100)
-	
-	def __unicode__(self):
-		return self.name
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
 
 class Zone(models.Model):
-	zip_code = models.CharField(max_length=3)
-	service_level = models.ForeignKey(ServiceLevel)
-	zone_number = models.CharField(max_length=3)
-	
-	def __unicode__(self):
-		return "Zip code: " + self.zip_code + ", Service Level: " + self.service_level
+    zip_code = models.CharField(max_length=3)
+    service_level = models.ForeignKey(ServiceLevel)
+    zone_number = models.CharField(max_length=3)
+
+    def __unicode__(self):
+        return "Zip code: " + self.zip_code + ", Service Level: " + self.service_level.name
+
 
 class Rate(models.Model):
-	weight = models.IntegerField()
-	service_level = models.ForeignKey(ServiceLevel)
-	zone_number = models.CharField(max_length=3)
-	price = models.IntegerField()
-	
-	def __unicode__(self):
-		return "Weight: " + self.weight + ", Service level: " + self.service_level + ", Zone: " + self.zone
+    weight = models.IntegerField()
+    service_level = models.ForeignKey(ServiceLevel)
+    zone_number = models.CharField(max_length=3)
+    price = models.IntegerField()
+
+    def __unicode__(self):
+        return "Weight: " + str(self.weight) + ", Service level: " + self.service_level.name + ", Zone: " + str(self.zone_number)
+
 
 class Shipment(models.Model):
-	date=models.DateField("Date", default=datetime.date.today)
- 	service_level = models.ForeignKey(ServiceLevel)
-	weight = models.IntegerField()
-        zone_number = models.CharField(max_length=3)
-        price = models.IntegerField()
+    date = models.DateField("Date", default=datetime.date.today)
+    service_level = models.ForeignKey(ServiceLevel)
+    weight = models.IntegerField()
+    zone_number = models.CharField(max_length=3)
+    price = models.IntegerField()
+
 class ShipmentForm(ModelForm):
-	class Meta:
-		model=Shipment
+    class Meta:
+        model = Shipment
 
--- Website/root/views.py
+++ Website/root/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_list_or_404
-from root.models import ServiceLevel
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from root.models import ServiceLevel, Zone
 
 def index(request):
-	service_levels = get_list_or_404(ServiceLevel)
-	return render(request,'index.html', {'service_levels': service_levels})
-	
+    service_levels = get_list_or_404(ServiceLevel)
+    return render(request, 'index.html', {'service_levels': service_levels})
+
+
 def search(request):
-	service_level = request.GET['service_level']
-	weight = request.GET['weight']
-	zip_code = request.GET['zip_code']
-	dictionary = {'service_level': service_level, 'weight': weight, 'zip_code': zip_code}
-	return render(request, 'search_result.html', dictionary)
+    service_level = request.GET['service_level']
+    weight = request.GET['weight']
+    zip_code = request.GET['zip_code']
+    zone = get_object_or_404(Zone, service_level=service_level, zip_code=zip_code)
+    return render(request, 'search_result.html', {'zone': zone})
